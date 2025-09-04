104cb4d8ba3e5ae9d83a09eb7ea230230846343a
James_Robins <james.robins@nottingham.ac.uk>
2025-09-04 18:51:46 +0100

    Update to Part 8 of the tutorial series

--- NeuralNetworkFromScrtach/ML1.py
+++ NeuralNetworkFromScrtach/ML1.py
@@ -111,7 +111,7 @@ Layer2.forward(Layer1.output)
 print(Layer2.output)'''
 
 
-'''Part 5 of Video - Actovation Functions'''
+'''Part 5 of Video - Actovation Functions
 # As all nodes are getting the same inputs, the inputs is still only 1 array of the same values
 import numpy as np
 np.random.seed(0)
@@ -150,4 +150,125 @@ activation1 = Activation_ReLU()
 Layer1.forward(X)
 activation1.forward(Layer1.output)
 
-print(activation1.output)
+print(activation1.output)'''
+
+
+'''Part 6 of Video - Softmax Activation
+# As all nodes are getting the same inputs, the inputs is still only 1 array of the same values
+import numpy as np
+np.random.seed(0)
+
+def spiral_data(points, classes):
+    X = np.zeros((points*classes, 2))
+    y = np.zeros(points*classes, dtype='uint8')
+    for class_number in range(classes):
+        ix = range(points*class_number, points*(class_number+1))
+        r = np.linspace(0.0, 1, points)  # radius
+        t = np.linspace(class_number*4, (class_number+1)*4, points) + np.random.randn(points)*0.2
+        X[ix] = np.c_[r*np.sin(t*2.5), r*np.cos(t*2.5)]
+        y[ix] = class_number
+    return X, y
+
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.1*(np.random.randn(n_inputs, n_neurons))
+        self.biases = np.zeros((1, n_neurons)) # Double brackets are important - Generate as a tuple
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights)+self.biases
+        
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0,inputs)
+
+class Activation_Softmax:
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs,axis=1,keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output = probabilities
+        
+X, y = spiral_data(points = 100,classes=3)
+
+dense1 = Layer_Dense(2,3) # input is 2 here because data (spiral data) is ony x, y values so 2 dimensions - the 3 is the number of nodes
+Activation1 = Activation_ReLU()
+
+dense2 = Layer_Dense(3,3)
+Activation2 = Activation_Softmax()
+
+dense1.forward(X)
+Activation1.forward(dense1.output)
+dense2.forward(Activation1.output)
+Activation2.forward(dense2.output)
+
+print(Activation2.output[:5])# just print first 5'''
+
+'''Part 7 of Video - Loss Calculation'''
+# As all nodes are getting the same inputs, the inputs is still only 1 array of the same values
+import numpy as np
+np.random.seed(0)
+
+def spiral_data(points, classes):
+    X = np.zeros((points*classes, 2))
+    y = np.zeros(points*classes, dtype='uint8')
+    for class_number in range(classes):
+        ix = range(points*class_number, points*(class_number+1))
+        r = np.linspace(0.0, 1, points)  # radius
+        t = np.linspace(class_number*4, (class_number+1)*4, points) + np.random.randn(points)*0.2
+        X[ix] = np.c_[r*np.sin(t*2.5), r*np.cos(t*2.5)]
+        y[ix] = class_number
+    return X, y
+
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.1*(np.random.randn(n_inputs, n_neurons))
+        self.biases = np.zeros((1, n_neurons)) # Double brackets are important - Generate as a tuple
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights)+self.biases
+        
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0,inputs)
+
+class Activation_Softmax:
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs,axis=1,keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output = probabilities
+        
+class loss:
+    def calculate(self, output, y):
+        sample_loss = self.forward(output, y)
+        data_loss = np.mean(sample_loss)
+        return data_loss
+    
+class loss_CategoricalCrossEntropy(loss):
+    def forward(self, y_pred, y_true):
+        samples = len(y_pred)
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1-1e-7)
+        
+        if len(y_true.shape) ==1:
+            correct_confidences = y_pred_clipped[range(samples), y_true]
+        elif len(y_true.shape) == 2:
+            correct_confidences = np.sum(y_pred_clipped*y_true, axis=1)
+        
+        negative_log_likelyhood = -np.log(correct_confidences)
+        return negative_log_likelyhood
+X, y = spiral_data(points = 100,classes=3)
+
+dense1 = Layer_Dense(2,3) # input is 2 here because data (spiral data) is ony x, y values so 2 dimensions - the 3 is the number of nodes
+Activation1 = Activation_ReLU()
+
+dense2 = Layer_Dense(3,3)
+Activation2 = Activation_Softmax()
+
+dense1.forward(X)
+Activation1.forward(dense1.output)
+dense2.forward(Activation1.output)
+Activation2.forward(dense2.output)
+
+print(Activation2.output[:5])# just print first 5
+
+lossfunction = loss_CategoricalCrossEntropy()
+
+lossvalue = lossfunction.calculate(Activation2.output, y)
+
+print(lossvalue)
